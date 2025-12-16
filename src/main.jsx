145c764cc23c8dbed0e9773d911bd9f14a84cb98
Chit Hsu Nyein <chitsunyein242@gmail.com>
2025-12-16 14:02:18 +0630

    update README.md , package.json, main.js'

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom'
 import { HelmetProvider } from 'react-helmet-async'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <StrictMode basename="/BuildDream">
     <BrowserRouter>
       <HelmetProvider>
         <App/>
